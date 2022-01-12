3bef4d40b5c18af52c0ae46599f1d6f6e7a16451
G <gurnank@outlook.com>
2022-01-12 16:19:58 +0000

    DCS-1395 update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.16"
-
-  kotlin("plugin.spring") version "1.5.31"
-  kotlin("plugin.jpa") version "1.5.31"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.1"
+  kotlin("plugin.spring") version "1.6.10"
+  kotlin("plugin.jpa") version "1.6.10"
 }
 
 configurations {
@@ -11,28 +10,24 @@ configurations {
 }
 
 dependencies {
-
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-
-  runtimeOnly("org.flywaydb:flyway-core")
-  runtimeOnly("org.postgresql:postgresql")
-
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.5.12")
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.12")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.12")
+  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.6.4")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.4")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.4")
 
-  testAnnotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+  runtimeOnly("com.h2database:h2:2.0.206")
+  runtimeOnly("org.flywaydb:flyway-core:8.4.1")
+  runtimeOnly("org.postgresql:postgresql")
 
-  testRuntimeOnly("com.h2database:h2")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("com.ninja-squad:springmockk:3.0.1")
+  testImplementation("com.ninja-squad:springmockk:3.1.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
 }
 
