8a9d16a0967e396c47e476d4c8f8179db0cbcbf6
Aled Evans <101728271+AledWEvans@users.noreply.github.com>
2025-05-22 09:57:24 +0100

    Added test for QL and changed schedule time to sunday 8 am (#175)
    * Added test for QL and changed schedule time to sunday 8 am
    
    * Added test for QL and changed schedule time to sunday 8 am
    
    * Added test for QL and changed schedule time to sunday 8 am

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
   testImplementation("com.ninja-squad:springmockk:4.0.2")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.6")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
+  testImplementation(kotlin("test"))
 }
 
 allOpen {
