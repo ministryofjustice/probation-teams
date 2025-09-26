1c6916f5465e734a6862a8c8fcb73e7dc668e22c
AledWEvans <aled.evans1@justice.gov.uk>
2025-09-26 12:16:12 +0100

    CVSL-3340 Security / Maintenance Rota - updates to cvl api for spring libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.1.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.0.2"
   kotlin("plugin.spring") version "2.1.21"
   kotlin("plugin.jpa") version "2.1.21"
 }
