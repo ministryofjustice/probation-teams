02fa9d119b4150dd55ced5f765f1bf491ad087c2
Aled Evans <101728271+AledWEvans@users.noreply.github.com>
2025-11-17 17:42:50 +0000

    Security / Maintenance Rota - updates deps (#186)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.4"
   id("org.owasp.dependencycheck") version "12.1.6"
   kotlin("plugin.spring") version "2.2.20"
   kotlin("plugin.jpa") version "2.2.20"
